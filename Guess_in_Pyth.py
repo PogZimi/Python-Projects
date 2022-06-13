d1dcba2aaa317c403a5d074a056c008111dfb877
Rayz_On <105559019+MiloPrysc@users.noreply.github.com>
2022-06-13 15:30:41 +0530

    Create Guess_in_Pyth.py

---
+++ Guess_in_Pyth.py
@@ -0,0 +1,27 @@
+import random
+
+#Variables initialization
+randnum = 0;
+ug = 0;
+guess = 0;
+
+randnum = random.randint(1, 100);    # Generates Random Number between 1 to 100
+# print(randnum);
+
+# Main loop
+while(ug!=randnum):
+
+     ug = int(input("Enter your guesses :  ")) # User Input 
+
+     if(ug == randnum):
+      print("You won!!!!");
+      print("Tries : ", guess)
+
+     elif(ug != randnum):
+      print("You Lose!!!!");
+     guess+=1;
+
+das = guess - 1;
+with open("Score.txt", "w") as f:
+    f.write(str(das));  # Creates a file with name Score.txt & pastes your score there 
+    
